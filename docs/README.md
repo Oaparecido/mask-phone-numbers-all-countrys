55657e00aa15758593e86d78b822e3d15a4db47a
Daniel Aparecido <daniel.aparecido@maquinadobem.com>
2020-12-04 21:53:06 -0300

    masks for europe union

---
+++ docs/README.md
@@ -0,0 +1,29 @@
+## Masks for the phone numbers!
+In this repository you find masks for the phone numbers in **all country's**, or should rs. 
+<br>
+I want to ask for your help in order to increase this list
+<br>
+If you want you can help me to increase the list, i would thank you very much for that!
+
+### Rules
+**Always follow this code pattern**
+* `country_code` - **DDI** from country.
+* `country_name` - Country **name** literally.
+
+**Rules mask**
+* `+###` - First DDI.
+* `####` For each **'#'** is one number.
+
+```php
+    'country_code' => [
+        'country' => 'country_name',
+        'mask' => '+## ## ####-####'
+    ]
+```
+
+---
+
+## Your have question?!
+
+Send me a **DM** in **instagram**:  
+[@aparecido.ts](https://www.instagram.com/aparecido.ts/)
---
+++ masks.php
@@ -0,0 +1,115 @@
+<?php
+
+return [
+    '55' => [
+        'country' => 'Brazil',
+        'mask' => [
+            10 => '+## (##) ####-####',
+            11 => '+## (##) #####-####',
+        ]
+    ],
+    '351' => [
+        'country' => 'Portugal',
+        'mask' => '+### ### ### ###'
+    ],
+    '49' => [
+        'country' => 'Germany',
+        'mask' => '+## ### ######'
+    ],
+    '43' => [
+        'country' => 'Austria',
+        'mask' => '+## ## ### ### ###-##'
+    ],
+    '32' => [
+        'country' => 'Belgium',
+        'mask' => '+## #### ######'
+    ],
+    '420' => [
+        'country' => 'Czechia',
+        'mask' => '+### ## #####'
+    ],
+    '359' => [
+        'country' => 'Bulgaria',
+        'mask' => '+### ##-######'
+    ],
+    '385' => [
+        'country' => 'Croatia',
+        'mask' => '+### ##-### ### ##'
+    ],
+    '45' => [
+        'country' => 'Denmark',
+        'mask' => '+## ########'
+    ],
+    '421' => [
+        'country' => 'Slovakia',
+        'mask' => '+### ## ########'
+    ],
+    '386' => [
+        'country' => 'Slovenia',
+        'mask' => '+### ########'
+    ],
+    '34' => [
+        'country' => 'Spain',
+        'mask' => '+## ### ###-###'
+    ],
+    '372' => [
+        'country' => 'Estonia',
+        'mask' => '+### ########'
+    ],
+    '358' => [
+        'country' => 'Finland',
+        'mask' => '+### ############'
+    ],
+    '33' => [
+        'country' => 'France',
+        'mask' => '+## ## ## ## ## ##'
+    ],
+    '30' => [
+        'country' => 'Greece',
+        'mask' => '+## ##########'
+    ],
+    '36' => [
+        'country' => 'Hungary',
+        'mask' => '+## ########'
+    ],
+    '353' => [
+        'country' => 'Ireland',
+        'mask' => '+### #########'
+    ],
+    '39' => [
+        'country' => 'Italy',
+        'mask' => '+## ### ####.###'
+    ],
+    '371' => [
+        'country' => 'Latvia',
+        'mask' => '+### ########'
+    ],
+    '370' => [
+        'country' => 'Lithuania',
+        'mask' => '+### ########'
+    ],
+    '352' => [
+        'country' => 'Luxembourg',
+        'mask' => '+### ### #########'
+    ],
+    '356' => [
+        'country' => 'Malta',
+        'mask' => '+### ## ####.####-##'
+    ],
+    '31' => [
+        'country' => 'Netherlands',
+        'mask' => '+## #########'
+    ],
+    '48' => [
+        'country' => 'Poland',
+        'mask' => '+## #########'
+    ],
+    '40' => [
+        'country' => 'Romania',
+        'mask' => '+## #########'
+    ],
+    '46' => [
+        'country' => 'Sweden',
+        'mask' => '+## #########'
+    ]
+];
